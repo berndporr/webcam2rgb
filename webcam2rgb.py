a926f7e5060d14b4645818260c5162a41e0e14c1
Bernd Porr <mail@berndporr.me.uk>
2020-10-28 16:18:28 +0000

    commented fs out but needs to be put back in later.

--- webcam2rgb.py
+++ webcam2rgb.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def calc_RGB():
     cam = cv2.VideoCapture(0)
-    fs = cam.get(cv2.CAP_PROP_FPS)
-    print("fs=",fs)
+    #fs = cam.get(cv2.CAP_PROP_FPS)
+    #print("fs=",fs)
     while True:
         ret_val, img = cam.read()
         h, w, c = img.shape
