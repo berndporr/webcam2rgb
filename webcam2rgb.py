dfba66a50a6b2df716d63498baa9a8668d8afcc4
jamielee183 <jamie.lee183@gmail.com>
2020-11-07 15:52:35 +0000

    added Direct show option - camera needs reopening

--- webcam2rgb.py
+++ webcam2rgb.py
@@ -10,9 +10,11 @@ class Webcam2rgb():
     def start(self, callback, cameraNumber=0, width = None, height = None, fps = None, directShow = False):
         self.callback = callback
         try:
-            self.cam = cv2.VideoCapture(cameraNumber + (cv2.CAP_DSHOW if directShow is True else 0))
+            self.cam = cv2.VideoCapture(cameraNumber + cv2.CAP_DSHOW if directShow else cv2.CAP_ANY) 
             if not self.cam.isOpened():
+                print('opening camera')
                 self.cam.open(0)
+                
             if width:
                 self.cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
             if height:
